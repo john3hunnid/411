28c4341d2ea36754e51c03fe34d36b95df3ba669
john3hunnid <johnad@bu.edu>
2024-10-07 16:17:09 -0400

    added update_migration_path_details

--- HW/HW3 Design/wildlife_tracker/migration_tracking/migration.py
+++ HW/HW3 Design/wildlife_tracker/migration_tracking/migration.py
@@ -18,9 +18,8 @@ class Migration:
     
     def remove_migration_path(path_id: int) -> None:
         pass
-   
-
-    
+    def update_migration_path_details(path_id: int, **kwargs) -> None:
+        pass
     def get_migration_paths() -> list[MigrationPath]:
         pass
     
